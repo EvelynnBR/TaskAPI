2d81093ca98206eedd7c37108b16e163f67a0f90
Evelynn <evelynnbr24@gmail.com>
2025-11-22 17:12:37 -0300

    criando rotas de login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { taskRoutes } from "@/routes/task-routes"
 import { userRoutes } from "@/routes/user-routes"
+import { sessionRoutes } from "./session-routes"
 
 export const routes = Router()
 
 routes.use("/tasks", taskRoutes)
 routes.use("/user", userRoutes)
+routes.use("/session", sessionRoutes)
