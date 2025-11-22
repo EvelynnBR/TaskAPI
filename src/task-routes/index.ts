7b419c71592f96aa52ac8da6367ab1621f2344a6
Evelynn <evelynnbr24@gmail.com>
2025-11-22 16:53:31 -0300

    renomeado

--- src/task-routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-import { taskRoutes } from "@/task-routes/task-routes"
-
-export const routes = Router()
-
-routes.use("/tasks", taskRoutes)
-
--- src/task-routes/task-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-import { TaskController } from "@/task-controllers/task-controller"
-
-export const taskRoutes = Router()
-const taskController = new TaskController()
-
-taskRoutes.post("/", taskController.create)
