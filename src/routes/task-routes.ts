ed1c6e5265ada6e0d0165c33fb26339cab8d4833
Evelynn <evelynnbr24@gmail.com>
2025-11-28 18:19:37 -0300

    passando rota para atualizar status

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { TaskController } from "@/controllers/task-controller"
+import { TaskStatusController } from "@/controllers/task-status-controller"
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
 
 export const taskRoutes = Router()
 const taskController = new TaskController()
+const taskStatusController = new TaskStatusController()
 
 taskRoutes.post("/", taskController.create)
 taskRoutes.get("/", ensureAuthenticated, taskController.index)
+taskRoutes.patch("/:id", ensureAuthenticated, taskStatusController.update)
