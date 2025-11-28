6eafdce23d63dd13777dd2522104d8b419248038
Evelynn <evelynnbr24@gmail.com>
2025-11-28 17:10:34 -0300

    passando autenticação na rota de index

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { TaskController } from "@/controllers/task-controller"
+import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
 
 export const taskRoutes = Router()
 const taskController = new TaskController()
 
 taskRoutes.post("/", taskController.create)
+taskRoutes.get("/", ensureAuthenticated, taskController.index)
