b22a4958241e098cb7a18f1f84f544e8056c23f2
Evelynn <evelynnbr24@gmail.com>
2025-11-28 23:27:45 -0300

    rota para deletar task

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -3,13 +3,16 @@ import { TaskController } from "@/controllers/task-controller"
 import { TaskStatusController } from "@/controllers/task-status-controller"
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
 import { TaskUpdateController } from "@/controllers/task-update-controller"
+import { TaskDeleteController } from "@/controllers/task-delete-controller"
 
 export const taskRoutes = Router()
 const taskController = new TaskController()
 const taskStatusController = new TaskStatusController()
 const taskUpdateController = new TaskUpdateController()
+const taskDeleteController = new TaskDeleteController()
 
 taskRoutes.post("/", taskController.create)
 taskRoutes.get("/", ensureAuthenticated, taskController.index)
 taskRoutes.patch("/:id", ensureAuthenticated, taskStatusController.update)
 taskRoutes.put("/:id", ensureAuthenticated, taskUpdateController.update)
+taskRoutes.delete("/:id", ensureAuthenticated, taskDeleteController.delete)
