30e524cf287e85b79c5ed38879e0556981a761c0
Evelynn <evelynnbr24@gmail.com>
2025-11-28 22:43:30 -0300

    criando a rota de atualização de task

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express"
 import { TaskController } from "@/controllers/task-controller"
 import { TaskStatusController } from "@/controllers/task-status-controller"
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
+import { TaskUpdateController } from "@/controllers/task-update-controller"
 
 export const taskRoutes = Router()
 const taskController = new TaskController()
 const taskStatusController = new TaskStatusController()
+const taskUpdateController = new TaskUpdateController()
 
 taskRoutes.post("/", taskController.create)
 taskRoutes.get("/", ensureAuthenticated, taskController.index)
 taskRoutes.patch("/:id", ensureAuthenticated, taskStatusController.update)
+taskRoutes.put("/:id", ensureAuthenticated, taskUpdateController.update)
